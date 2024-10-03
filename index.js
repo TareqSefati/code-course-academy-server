c1978424c19a8548fb69eefaabe506e453b5e89a
Tareq Sefati <tareq.sefati@gmail.com>
2024-10-03 19:00:32 +0600

    Add courseDetails.json file and Implement two API end point for all products & specific product.

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+const courseData = require("./data/courseDetails.json");
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+    res.send("Code Course Academy Server is Up & Running");
+});
+
+app.get("/api/products", (req, res) => {
+    res.send(courseData);
+});
+
+app.get("/api/products/:id", (req, res) => {
+    const id = req.params.id;
+    console.log(id);
+    const courseById = courseData.filter((item) => item.course_id === id);
+    res.send(courseById);
+});
+
+app.listen(port, () => {
+    console.log(`Code Course Academy Server is Up & Running on Port:  ${port}`);
+});
